f948fa493795fae90d60f0248b4a3d2434333a54
Joern Bungartz <joern@bungartz.pm>
2015-04-12 20:19:16 +0000

    - set static_root in settings.py

--- sc_frontend/sc_frontend/settings.py
+++ sc_frontend/sc_frontend/settings.py
@@ -89,6 +89,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/simplecam.de/static'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
