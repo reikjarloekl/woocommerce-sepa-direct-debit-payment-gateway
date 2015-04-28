cc381135c1cc8de87eeeff835ac396573150e867
Joern Bungartz <joern@bungartz.pm>
2015-04-28 21:27:40 +0000

    - show referrer.

--- sc_frontend/front/views.py
+++ sc_frontend/front/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.clickjacking import xframe_options_exempt
+from django.contrib import messages
 from front.models import Camera
 
 @xframe_options_exempt
@@ -11,4 +12,5 @@ def print_cookies(request):
     context = RequestContext(request, {
         'cameras': cameras,
     })
+    messages.info(request, request.META['HTTP_REFERER'])
     return render_to_response('front/index.html', context)
