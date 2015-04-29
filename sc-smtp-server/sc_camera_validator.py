1e522491a8a038c49532760c6ee70aae61a882c1
Joern Bungartz <joern@bungartz.pm>
2015-04-29 20:25:44 +0000

    - added logging for log-in attempts from unkown cameras.

--- sc-smtp-server/sc_camera_validator.py
+++ sc-smtp-server/sc_camera_validator.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import hmac
+import logging
 import re
 from slimta.edge.smtp import SmtpValidators
 from slimta.smtp.auth import CredentialsInvalidError, Auth
@@ -9,12 +10,14 @@ from sc_camera_information.sc_camera_information import ScCameraInformation
 import settings
 
 __author__ = 'Joern'
+logger = logging.getLogger(__name__)
 
 class ScValidators(SmtpValidators):
     def handle_rcpt(self, reply, recipient):
         if recipient != "hub@simplecam.de":
             reply.code = '550'
             reply.message = '5.7.1 Recipient <{0}> Not allowed'.format(recipient)
+            logger.info('Sending attempted to other recipient than "hub@simplecam.de".')
             return
 
 
@@ -24,6 +27,7 @@ class ScAuth(Auth):
         try:
             ScCameraInformation(camera_id)
         except IndexError:
+            logger.info('Log-in attempted from unknown camera ({}).'.format(user_id))
             raise CredentialsInvalidError()
 
         expected_pw = base64.b64encode(hmac.new(settings.SECRET_KEY, camera_id, hashlib.sha256).digest())[:20]
@@ -40,23 +44,8 @@ class ScAuth(Auth):
         pattern = re.compile('\d*@simplecam.de')
         if pattern.match(username):
             return self.get_password(username), username
+        logger.info('Log-in attempted from unknown camera ({}).'.format(username))
         raise CredentialsInvalidError()
 
     def get_available_mechanisms(self, connection_secure=False):
         return [Login]
-
-
-class ScCameraValidator(object):
-    def validate(self, username, password):
-
-        camera_id = username.split('@')[0]
-        try:
-            ScCameraInformation(camera_id)
-        except IndexError:
-            return False
-
-        expected_pw = base64.b64encode(hmac.new(settings.SECRET_KEY, camera_id, hashlib.sha256).digest())[:20]
-
-        if password == expected_pw:
-            return True
-        return False
