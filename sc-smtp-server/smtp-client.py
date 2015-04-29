b44c89d668fce1c6f1df17958390970581317ec1
Joern Bungartz <joern@bungartz.pm>
2015-04-29 18:57:20 +0000

    - updated requirements and smtp test client

--- sc-smtp-server/smtp-client.py
+++ sc-smtp-server/smtp-client.py
@@ -1,10 +1,11 @@
 import smtplib
 
+#with open("test/mail-no-img.txt", "rb") as fil:
 with open("test/mail.txt", "rb") as fil:
         msg = fil.read()
 
 server = smtplib.SMTP('localhost', port=1025)
 server.set_debuglevel(1)
-server.login('1@simplecam.de', 'Nkfm8201/ltNdjdaytrg')
-server.sendmail('1@simplecam.de', ['hub@simplecam.de'], msg)
+server.login('2@simplecam.de', 'nQ7rrIax22TBHivt+avO')
+server.sendmail('2@simplecam.de', ['hub@simplecam.de'], msg)
 server.quit()
