39079731cd781ef596fdbeb8f4983fe5dcda3a42
Joern Bungartz <joern@bungartz.pm>
2015-05-26 21:31:25 +0000

    - added wordpress db - linked sc-logo in confirmation email.

--- db/mysql-setup.sql
+++ db/mysql-setup.sql
@@ -6,4 +6,8 @@ CREATE USER 'sc_smtp'@'localhost' IDENTIFIED BY 'k4VczgPtBwpHVdjFeFvi';
 GRANT SELECT ON simplecam.* TO 'sc_smtp'@'localhost';
 GRANT INSERT ON simplecam.front_image TO 'sc_smtp'@'localhost';
 
+CREATE DATABASE wordpress CHARACTER SET utf8;
+CREATE USER 'wp'@'localhost' IDENTIFIED BY '51OJIB1cEwbia5UkQX2P'
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'localhost';
+
 flush privileges;
