156ce92d1b2c5a76cc869bddd3af26651a983694
Joern Bungartz <joern@bungartz.pm>
2018-02-05 14:14:27 +0000

    Added french translation. Release 1.6.9

--- package.bat
+++ package.bat
@@ -1,5 +1,5 @@
 del sepa-direct-debit.zip
 copy "assets\SEPA Payment Gateway for WooCommerce.pdf" .
-7za a sepa-direct-debit.zip "SEPA Payment Gateway for WooCommerce.pdf" css js languages lib sepa-direct-debit.php WC_Gateway_SEPA_Direct_Debit.php sepa-checks.php
+7z a sepa-direct-debit.zip "SEPA Payment Gateway for WooCommerce.pdf" css js languages lib sepa-direct-debit.php WC_Gateway_SEPA_Direct_Debit.php sepa-checks.php
 del "SEPA Payment Gateway for WooCommerce.pdf"
 pause
